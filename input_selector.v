c89510d3787cfae510365b416b16bc203f635a2d
drhl <drhl@pro.wp.pl>
2023-04-26 10:20:46 +0200

    Comments added

--- input_selector.v
+++ input_selector.v
@@ -1,22 +1,32 @@
-module input_selectpr(
-	inout in_clk,
-	input in_bck_1,
-	input in_le_1,
-	input in_data_1,
-	input in_bck_2,
-	input in_le_2,
-	input in_data_2,
-	input in_button,
-	output out_bck,
-	output out_le,
-	output out_data,
-	output out_fixed_1,
-	output out_fixed_0
-	);
+/*
 
-reg [1:0] 	selected_input;
-reg [1:0] 	counter_enable;
-reg [28:0]  counter;
+Module for I2S input selection. 
+After every change we are waiting ca. 1s to allow the next change.
+
+Author: drhl
+Year: 2023
+
+*/
+
+module input_selectpr(
+	inout in_clk,		// system clock, 50 MHz
+	input in_bck_1,	// I2S SCK/BCK input 1
+	input in_le_1,		// I2S LE input 1
+	input in_data_1,	// I2S DATA input 1
+	input in_bck_2,	// I2S SCK/BCK input 2
+	input in_le_2,		// I2S LE input 2
+	input in_data_2,	// I2S DATA input 2
+	input in_button,	// push button input
+	output out_bck,	// I2S SCK/BCK output
+	output out_le,		// I2S LE output
+	output out_data,	// I2S DATA output
+	output out_fixed_1,	// output with high state - helper for push button
+	output out_fixed_0	// output wit low state - helper for push button
+);
+
+reg [1:0] 	selected_input; // flag indicating which input is selected
+reg [1:0] 	counter_enable; // flag asserted when counter is active - right after input selection change
+reg [28:0]  counter;			 // counter to hold next input change for ca. 1s
 
 initial begin
 	selected_input <= 0;
@@ -25,6 +35,7 @@ initial begin
 end
 
 always @(posedge in_clk) begin
+	// in_clk - 50MHz
 	if(counter_enable == 1 && counter <= 50000000) begin
 		counter <= counter + 1;
 	end else if(counter_enable == 0) begin
@@ -33,15 +44,19 @@ always @(posedge in_clk) begin
 end
 
 always @(posedge in_button) begin
+	// push button pressed and input change is allowed
 	if(in_button == 1 && counter_enable == 0) begin
+	   // change input
 		if(selected_input == 0) begin
 			selected_input <= 1;
 		end else begin
 			selected_input <= 0;
 		end
+		// lock next input change for ca. 1s
 		counter_enable <= 1;
 	end else begin
 		if(counter >= 50000000) begin
+			// unlock next input change
 			counter_enable <= 0;
 		end
 	end
--- mute_sck.v
+++ mute_sck.v
@@ -10,8 +10,7 @@ Year: 2023
 module mute_sck(
 	input in_clk, // system clock, 50 MHz
 	input in_sck, // I2S SCK/BCK input
-	input in_le,  // I2S LE input
-	output reg out_sck_mute, // I2S SCK/BCK output with mute function 
+	output out_sck_mute, // I2S SCK/BCK output with mute function 
 	output reg out_mute_led  // status LED for muted state
 ); 
 
@@ -22,41 +21,34 @@ reg [1:0] enable;	// BCK output enable flag
 initial begin
 	counter <= 0;
 	seconds <= 0;
-	out_sck_mute <= 0;
 	out_mute_led <= 1;
 	enable <= 0;
 end
 
+// assign input BCK to output BCK only when BCK output flag is asserted
+assign out_sck_mute = enable == 1 ? in_sck : 0;
+
 always @(posedge in_clk)
 begin
-   // 50MHz
-	if(enable == 1 && seconds < 30)
+   // wait ca. 30s
+	if(seconds < 30)
 		begin
-			if(counter == 50000000) 
-				begin
-					counter <= 0;
-					seconds <= seconds + 1;
-				end
-			else
+			// in_clk - 50MHz
+			if(counter >= 50000000) begin
+				counter <= 0;
+				seconds <= seconds + 1;
+			end else begin
 				counter <= counter + 1;
+			end
 		end
 	else
 		begin
-			if(out_mute_led == 1) 
-			begin
+			if(enable == 0) begin
+				// turn off mute LED
 				out_mute_led <= 0;
+				// enable BCK/SCK output
+				enable <= 1;
 			end
-			out_sck_mute <= in_sck;
-		end
-end
-
-
-always @(posedge in_le)
-begin
-   // 50MHz
-	if(seconds == 30)
-		begin
-			enable <= 1;
 		end
 end
 
